c2e95313d2771cc81a31e8c6268a11946d55fc01
YunusGafur <127ygafur@frhsd.com>
2025-11-24 14:06:25 -0500

    333

--- js/java.js
+++ js/java.js
@@ -2,14 +2,17 @@ const submit = document.getElementById('submitBtn');
 const nameInput = document.getElementById('nameInput');
 const ageInput = document.getElementById('ageInput');
 const colorInput = document.getElementById('colorInput');
+const petInputs = document.querySelectorAll('input[type="radio"]');
 const output = document.getElementById('output');
 
 submit.addEventListener('click', function () {
-    const name = nameInput.value.trim();
+    const name = nameInput.value;
     const age = ageInput.value;
     const color = colorInput.value;
+    let pet = 'dog, cat, fish';
 
     output.innerHTML = 'Name: ' + name +
-        '<br>Age: ' + age +
-        '<br>Favorite Color: ' + color;
-});
+        ' Age: ' + age +
+        ' Favorite Color: ' + color + ' Favorite Pet: ' 
+        + pet;
+    });
